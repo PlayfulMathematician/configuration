0fbb49d7847649a4725281a4453eb3a27419f447
PlayfulMathematician <me@playfulmathematician.com>
2026-01-24 16:08:33 +0800

    removed org mode

--- neovim/lua/keymaps/org.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.setup()
-	local map = vim.keymap.set
-	map("n", "<leader>oa", "<cmd>Org agenda<CR>", { desc = "Org Agenda" })
-	map("n", "<leader>oc", "<cmd>Org capture<CR>", { desc = "Org Capture" })
-end
-
-return M
