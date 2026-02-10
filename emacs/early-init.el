340b958c286ef248f76b076b3183df3b3727033d
Playful Mathematician <me@playfulmathematician.com>
2026-02-10 10:40:42 +0800

    basic cleanup: first version

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,1 +1,2 @@
+;;;;* early-init.el
 (setq package-enable-at-startup nil)
--- emacs/init.el
+++ emacs/init.el
@@ -1,34 +1,26 @@
+;;;;* init.el
 ;;;* straight-el
 (defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" (or (bound-and-true-p straight-base-dir) user-emacs-directory)))
+(bootstrap-version 7))
+(unless (file-exists-p bootstrap-file)
+(with-current-buffer (url-retrieve-synchronously "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el" 'silent 'inhibit-cookies)
+(goto-char (point-max))
+(eval-print-last-sexp)))
+(load bootstrap-file nil 'nomessage))
 ;;;* package installation
-(use-package catppuccin-theme :ensure t :demand t  :straight t)
-(use-package elcord :ensure t :demand t :straight t)
-;; (use-package evil
-;;  :ensure t
-;;  :init
-;;  (setq evil-want-integration t)
-;;  (setq evil-want-keybinding nil)
-;;  (setq evil-undo-system 'undo-redo)
-;;  :config
-;;   (evil-mode 1)  :straight t)
-(use-package org-bullets :straight t)
+(use-package catppuccin-theme
+:ensure t
+:demand t
+:straight t)
+(use-package elcord
+:ensure t
+:demand t
+:straight t)
+(use-package org-bullets
+:straight t)
 ;;;* require
-(with-eval-after-load 'org
-  (require 'org-agenda))
+(with-eval-after-load 'org (require 'org-agenda))
 (require 'org-crypt)
 (require 'ox-md)
 (require 'elcord)
@@ -38,24 +30,34 @@
 
 ;;;* custom functions
 (defun my/open-index-org ()
-  (interactive)
-  (find-file (expand-file-name "~/stuff_bin/documents/org/index.org")))
+(interactive)
+(find-file (expand-file-name "~/stuff_bin/documents/org/index.org")))
 (defun my/open-init-el ()
-  (interactive)
-  (find-file (expand-file-name "init.el" user-emacs-directory)))
+(interactive)
+(find-file (expand-file-name "init.el" user-emacs-directory)))
 (defun my/init-el-p ()
-  (and buffer-file-name
-       (string-equal (file-truename buffer-file-name)
-                     (file-truename (expand-file-name "init.el" user-emacs-directory)))))
+(and buffer-file-name (string-equal (file-truename buffer-file-name)
+(file-truename (expand-file-name "init.el" user-emacs-directory)))))
 (defun my/org-hook ()
-  (org-indent-mode)
-  (org-overview)
-  (org-bullets-mode 1)
- )
-;;;* catppuccin theme
-(load-theme 'catppuccin :no-confirm)
+(org-indent-mode)
+(org-overview)
+(org-bullets-mode 1))
 
-;;;* keybinds 
+(defun my/init-el-hook ()
+(setq-local outline-regexp ";;;\\*+ ")
+(outline-minor-mode 1)
+(save-excursion (goto-char (point-min))
+(when (re-search-forward "^;;;\\*+ straight-el" nil t)
+(outline-hide-subtree))))
+(defun my/elisp-hook ()
+(when (my/init-el-p)
+(my/init-el-hook)))
+
+;;;* catppuccin theme
+(load-theme 'catppuccin
+:no-confirm)
+
+;;;* keybinds
 (global-set-key (kbd "C-c j") #'my/open-index-org)
 (global-set-key (kbd "C-c g") #'my/open-init-el)
 (global-set-key (kbd "C-c a") #'org-agenda)
@@ -74,51 +76,37 @@
 ;;;* word wrap and fonts
 (setq word-wrap t)
 (setq-default truncate-lines nil)
-(set-face-attribute 'default nil :height 240)
+(set-face-attribute 'default nil
+:height 240)
 
 ;;;* org hooks
 (add-hook 'org-mode-hook #'my/org-hook)
 
-;;;* hooks to hide elpaca (i hate elpaca)  
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (when (my/init-el-p)
-              (setq-local outline-regexp ";;;\\*+ ")
-              (outline-minor-mode 1))))
+;;;* hooks to hide elpaca (i hate elpaca)
+(add-hook 'emacs-lisp-mode-hook #'my/elisp-hook)
 
-(add-hook 'outline-minor-mode-hook
-          (lambda ()
-            (when (my/init-el-p)
-              (save-excursion
-                (goto-char (point-min))
-                (when (re-search-forward "^;;;\\*+ straight-el" nil t)
-                  (outline-hide-subtree))))))
 
 ;;;* mode line
-(setq mode-line-position
-      '((line-number-mode ("%l" (column-number-mode ":%c")))
-        (:eval (format " W:%d"
-                       (count-words (point-min) (point-max))))))
+(setq mode-line-position '((line-number-mode ("%l" (column-number-mode ":%c")))
+(:eval (format " W:%d" (count-words (point-min)
+(point-max))))))
 
 ;;;* backup/autosave
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 
 ;;;* initial buffer
-(setq initial-buffer-choice
-      (expand-file-name "~/stuff_bin/documents/org/index.org"))
+(setq initial-buffer-choice (expand-file-name "~/stuff_bin/documents/org/index.org"))
 
 ;;;* org file
-(setq org-agenda-files
-      '("~/stuff_bin/documents/org/index.org"))
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)")))
+(setq org-agenda-files '("~/stuff_bin/documents/org/index.org"))
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)")))
 (org-crypt-use-before-save-magic)
 (setq org-crypt-key nil)
 (setq org-tags-exclude-from-inheritance '("crypt"))
 (setq epa-file-cache-passphrase-for-symmetric-encryption t)
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
-(setq org-startup-with-inline-images t) 
+(setq org-startup-with-inline-images t)
 ;;;* misc
 (setq-default cursor-type 'bar)
